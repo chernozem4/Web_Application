233ecdb8061cd53467758a3cd01d874fde8fd571
chernozem4 <gaftaf2034@gmail.com>
2024-12-05 15:31:14 +0600

    Добавьте файлы проекта.

---
+++ WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly ApplicationDbContext context;
+
+        public ProductsController(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = context.Products.ToList();
+            return View(products);
+        }
+    }
+}
